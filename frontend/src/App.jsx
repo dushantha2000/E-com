1d348ac45ec780da82aa0f39d75aa7612134d30d
dushantha <majithdushantha@gmail.com>
2025-01-17 18:52:41 +0530

    Describe your changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./components/Home";
 import Shop from "./components/Shop";
+import ShopNew from "./components/ShopNew";
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
+          <Route path="/shopNew" element={<ShopNew />} />
         </Routes>
       </BrowserRouter>
     </>
