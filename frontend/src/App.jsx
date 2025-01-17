d1c4bb65a847a73e0b5bc34aa9a51e518994ac55
dushantha <majithdushantha@gmail.com>
2025-01-17 13:01:29 +0530

    frontend home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./components/Home";
 import Shop from "./components/Shop";
+import './App.css'
 
 function App() {
   return (
